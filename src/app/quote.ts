69e4e6a5fff086e4ee07e3e661992753e992256f
NelsonKioko <kiokonelson2@gmail.com>
2019-03-25 02:06:16 +0300

    Convert the form div and the quotes div into columns

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,7 @@ export class Quote {
     public upvote:number;
     public downvote:number;
     public showQuoteDetails:boolean;
-    constructor(public id:number, public name:string, public quote:string, public completeDate:Date){
+    constructor(public id:number, public name:string, public quote:string, public writtenDate:Date){
         this.showQuoteDetails = false;
     }
   
